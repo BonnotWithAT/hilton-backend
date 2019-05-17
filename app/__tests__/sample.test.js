d3b10e31bb8842329426aa2521d7ef918e01f1f4
John Bonnot <johnbonnot@gmail.com>
2019-05-17 09:43:44 -0500

    Saving for branch to fix test running

--- app/__tests__/sample.test.js
+++ app/__tests__/sample.test.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../components/App';
+
 describe('sample test 101', () => {
-  console.log('Tests running');
+  it('renders without crashing', () => {
+    shallow(<App />)
+  })
 });
